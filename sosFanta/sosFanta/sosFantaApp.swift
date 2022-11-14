ded35f75b5aada6540b988e704ec948a8a043926
leodan98 <ldaniele22@fed.idserve.net>
2022-11-14 14:47:50 +0100

    first commit

---
+++ sosFanta/sosFanta/sosFantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sosFantaApp.swift
+//  sosFanta
+//
+//  Created by Leonardo Daniele on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct sosFantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
